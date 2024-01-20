97f7aa745f8daf855d1fdcea749278288e455e9c
wkingsurin <wkingsurin@gmail.com>
2024-01-20 22:07:54 +0500

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import classes from "./App.module.scss";
+
+export default function App() {
+  console.log(classes);
+
+  return (
+    <div className="wrapper">
+      {/* <Header /> */}
+      {/* <Main /> */}
+      {/* <Footer /> */}
+    </div>
+  );
+}
